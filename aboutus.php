cd911fcb95d6b0eff4ba806199746a5064dd1caa
Margaret-Kinuthia <87829995+Margaret-Kinuthia@users.noreply.github.com>
2021-08-31 03:40:23 +0300

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,193 @@
+<h1>About Us</h1>
+  <div class="intro">
+    <div class="container">
+      <div class="row row-eq-height">
+
+        <!-- Intro Content -->
+        <div class="col-lg-6">
+          <div class="intro_content">
+            <div class="section_title">
+              <div>Hello</div>
+              <h1>About Tilisi Hotel</h1>
+            </div>
+            <div class="intro_text">
+              <p>Tilisi Hotel is a home away from home. It is a pocket friendly hotel that attracts amazing customers. The hotel offers accomodation and catering facilities for customers who can be locals or tourists. The hotel provides awide range of activities, from swimming and gymnasium to spas and other relaxing activites.</p>
+            </div>
+           <!--  <div class="button intro_button"><a href="#">read more</a></div> -->
+          </div>
+        </div>
+
+        <!-- Intro Image -->
+        <div class="col-lg-6">
+          <div class="intro_image">
+            <div class="background_image" style="background-image:url(images/intro.jpg)"></div>
+            <img src="images/intro.jpg" alt="">
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <!-- Offering -->
+
+  <div class="offering">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="section_title text-center">
+            <div>Resort</div>
+            <h1>What we offer</h1>
+          </div>
+        </div>
+      </div>
+      <div class="row offering_row">
+        
+        <!-- Offer Item -->
+        <div class="col-xl-4 col-md-6">
+          <div class="offer">
+            <div class="offer_image"><img src="images/offer_1.jpg" alt=""></div>
+            <div class="offer_content text-center">
+              <div class="offer_title"><h3>Outdoor Pool</h3></div>
+              <div class="offer_text">
+                <p>A beautiful pool outside incase you want to get some sun.</p>
+              </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+            </div>
+          </div>
+        </div>
+
+        <!-- Offer Item -->
+        <div class="col-xl-4 col-md-6">
+          <div class="offer">
+            <div class="offer_image"><img src="images/offer_2.jpg" alt=""></div>
+            <div class="offer_content text-center">
+              <div class="offer_title"><h3>Indoor Pool</h3></div>
+              <div class="offer_text">
+                <p>A beautiful pool inside as well incase you do not want to go outside.</p>
+              </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+            </div>
+          </div>
+        </div>
+
+        <!-- Offer Item -->
+        <div class="col-xl-4 col-md-6">
+          <div class="offer">
+            <div class="offer_image"><img src="images/offer_3.jpg" alt=""></div>
+            <div class="offer_content text-center">
+              <div class="offer_title"><h3>Spa Centre</h3></div>
+              <div class="offer_text">
+                <p>A spa zone to just relax.</p>
+              </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+            </div>
+          </div>
+        </div>
+
+        <!-- Offer Item -->
+        <div class="col-xl-4 col-md-6">
+          <div class="offer">
+            <div class="offer_image"><img src="images/offer_4.jpg" alt=""></div>
+            <div class="offer_content text-center">
+              <div class="offer_title"><h3>Sports Area</h3></div>
+              <div class="offer_text">
+                <p>Sports area to play some games with family and friends.</p>
+              </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+            </div>
+          </div>
+        </div>
+
+        <!-- Offer Item -->
+        <div class="col-xl-4 col-md-6">
+          <div class="offer">
+            <div class="offer_image"><img src="images/offer_5.jpg" alt=""></div>
+            <div class="offer_content text-center">
+              <div class="offer_title"><h3>Restaurant</h3></div>
+              <div class="offer_text">
+                <p>Great food to get you in a great mood.</p>
+              </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+            </div>
+          </div>
+        </div>
+
+        <!-- Offer Item -->
+        <div class="col-xl-4 col-md-6">
+          <div class="offer">
+            <div class="offer_image"><img src="images/offer_6.jpg" alt=""></div>
+            <div class="offer_content text-center">
+              <div class="offer_title"><h3>Skybar</h3></div>
+              <div class="offer_text">
+                <p>Watch the spectular sunrises and sunsets in the skybar.</p>
+              </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <!-- Discover -->
+
+  <div class="discover">
+
+    <!-- Discover Content -->
+    <div class="discover_content">
+      <div class="container">
+        <div class="row">
+
+          <!-- Discover Content -->
+          <div class="col-xl-5 col-lg-6">
+            <div class="section_title">
+              <div>Hotel</div>
+              <h1>Discover Tilisi Hotel</h1>
+            </div>
+            <div class="discover_highlight">
+              <p>Discover all about this beautiful hotel. This is definitely the place to be. Home away from home. </p>
+            </div>
+            <div class="discover_text">
+              <p>Tilisi Hotel was established in 1993. It is a place where people come to relax and catch a break from the day to day life. The hotel has a lot of activities to make sure our guests are never bored. But also has places to relax and kick back for our guests who just want peace and quiet.</p>
+            </div>
+            <div class="button discover_button"><a href="#">discover</a></div>
+          </div>
+
+          <!-- Milestones -->
+          <div class="col-lg-6 offset-xl-1">
+            <div class="milestones d-flex flex-row align-items-start justify-content-start flex-wrap">
+              
+              <!-- Milestone -->
+              <div class="milestone">
+                <div class="milestone_counter" data-end-value="75">0</div>
+                <div class="milestone_text">Deluxe Rooms</div>
+              </div>
+
+              <!-- Milestone -->
+              <div class="milestone">
+                <div class="milestone_counter" data-end-value="110">0</div>
+                <div class="milestone_text">Years of Experience</div>
+              </div>
+
+              <!-- Milestone -->
+              <div class="milestone">
+                <div class="milestone_counter" data-end-value="31">0</div>
+                <div class="milestone_text">Awards Won</div>
+              </div>
+
+              <!-- Milestone -->
+              <div class="milestone">
+                <div class="milestone_counter" data-end-value="51" data-sign-after="k">0</div>
+                <div class="milestone_text">Happy Clients</div>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+  </div>
---
+++ largeimg.php
@@ -0,0 +1,21 @@
+  
+            <!-- Contact Details Column -->
+            <div class="col-lg-8">
+                <h3>Contact Details</h3>
+                <p>
+                    Mombasa<br>Kenya<br>
+                </p>
+                <p><i class="fa fa-phone"></i> 
+                    <abbr title="Phone">P</abbr>: (123) 456-7890</p>
+                <p><i class="fa fa-envelope-o"></i> 
+                    <abbr title="Email">E</abbr>: <a href="mailto:name@example.com">name@example.com</a>
+                </p>
+                <p><i class="fa fa-clock-o"></i> 
+                    <abbr title="Hours">H</abbr>: Monday - Friday: 9:00 AM to 5:00 PM</p>
+           
+            </div>
+              <div class="col-lg-4"> 
+        <div class="row">
+          <?php  require_once('sidebar.php') ; ?>
+        </div>
+      </div>
